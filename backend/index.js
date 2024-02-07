f6c4a23f8d8e7d1cc9d3336e509dc735d5497249
bhatia4404 <shivambhatia4404@gmail.com>
2024-02-07 20:13:43 +0530

    Added package.json files to backend folder

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cors());
-const userRouter = require("../backend/routes/user.js");
+const userRouter = require("./routes/user.js");
 app.use("/user", userRouter);
 const PORT = 3000;
 app.listen(PORT, function () {
